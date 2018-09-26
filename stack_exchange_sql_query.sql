9bbea490abecc4a5a31d847cd1d784ad992e70d2
Mithra Kruthiventi <42447448+mithrakrk@users.noreply.github.com>
2018-09-26 18:38:40 +0530

    Query to Get Stack Exchange Data Explorer data
    Can replace 'python' with other tag

---
+++ stack_exchange_sql_query.sql
@@ -0,0 +1,19 @@
+SELECT TOP 50000 *
+FROM 
+(
+  SELECT Id, PostTypeId, Body, Title, Score, CreationDate, ClosedDate
+  FROM Posts
+  WHERE PostTypeId = 1
+    AND CreationDate BETWEEN '2017-01-01' AND '2017-12-31'
+)AS post
+INNER JOIN
+(
+  SELECT ptag.PostId as PostId, tag.TagName as Tag
+  FROM PostTags AS ptag
+  INNER JOIN Tags as tag
+    ON tag.Id = ptag.TagId
+    AND tag.TagName = 'python'
+  GROUP BY ptag.PostID, tag.TagName
+) AS postTag
+ON post.Id = postTag.PostId
+ORDER BY post.Score
